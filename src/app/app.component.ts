892a596e1c0eeda5f01c60620e328d44c837c012
LimirioGuimaraes <limirioguimaraes@gmail.com>
2024-09-03 18:00:01 -0300

    Adicionado Front-end do footer e do header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
+import { Navbar1Component } from "./components/navbar1/navbar1.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, Navbar1Component, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
